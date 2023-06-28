933c17f07303920b4f7cfd6b1f3dbd960a0f4299
Knb619 <y.kanebako@neuro-tech.jp>
2023-06-28 16:45:23 +0900

    情報登録フォーム作成

---
+++ SampleWeb/src/main/java/com/example/demo/RegisterBean.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//@Getter
+//@Setter
+public class RegisterBean {
+//  private String name;
+//  private String age;
+	public String name;
+	public String age; 
+}
